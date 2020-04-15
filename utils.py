7a51887556e518fe03cf0d8e8eb7c2a4cf3a10ea
Jcvita <jcvita2012@gmail.com>
2020-04-15 07:10:01 -0400

    some todos

--- utils.py
+++ utils.py
@@ -27,6 +27,13 @@ def process_input(query: str, wake_word=True, quiet_mode=False):
                         return change_volume(int(word), quiet_mode)
                 else:
                     continue
+        if cmd[1] == 'alarm':
+            if cmd[2] == 'for' or cmd[2] == 'at':
+                pass  # TODO set an action to happen at a certain datetime object (maybe create a thread)
+            if cmd[2] == 'sound':
+                pass  # TODO store a variable somewhere of a sound
+    if cmd[0] == 'remove' or cmd[0] == 'delete':
+
     if cmd[0] in PLAY_MUSIC_KEYWORDS:
         pass
     return make_response('Didn\'t understand arguments\n', 200)
@@ -36,7 +43,7 @@ def change_volume(vol: int, quiet_mode: bool):
     global volume
     volume = vol
     if not quiet_mode:
-        speak(f'set volume to {volume}')
+        speak(f'Set volume to {volume}')
     return make_response(f'Successfully changed volume to {volume}', 200)
 
 
@@ -49,7 +56,7 @@ def speak(text):
 
 
 def song_title_to_uri(title, artist):
-    pass
+    spotipy.Spotify.s
 
 
 def play_song_with_spotify(uris: list):
